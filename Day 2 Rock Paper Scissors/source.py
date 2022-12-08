210f4a1c112e2ad6f6bfe16a6e482c3648e4af20
Mikael Särkiniemi <mikaels1997@gmail.com>
2022-12-08 22:06:54 +0200

    Day 2

---
+++ Day 2 Rock Paper Scissors/source.py
@@ -0,0 +1,49 @@
+points = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3
+}
+
+game_logic = {  # Opponent input: (losing player move, draw player move, winning player move)
+    "A": ("Z", "X", "Y"),
+    "B": ("X", "Y", "Z"),
+    "C": ("Y", "Z", "X")
+}
+
+def calculate_points(strategy):
+    total_score = 0
+    for inputs in strategy:
+        result_points = 0
+        if game_logic[inputs[0]][2] == inputs[1]:   # Winning move
+            result_points = 6
+        if game_logic[inputs[0]][1] == inputs[1]:   # Drawing move
+            result_points = 3
+        total_score += points[inputs[1]] + result_points
+    return total_score
+
+def secret_strategy(strategy):
+    total_score = 0
+    for inputs in strategy:
+        player_input = game_logic[inputs[0]][0]
+        if inputs[1] == "Y":
+            total_score += 3
+            player_input = game_logic[inputs[0]][1] # Drawing move
+        elif inputs[1] == "Z":
+            total_score += 6
+            player_input = game_logic[inputs[0]][2] # Winning move
+        total_score += points[player_input]
+    return total_score
+
+def read_input(path):
+    game_list = []
+    with open(path) as f:
+        for line in f.readlines():
+            inputs = line.split(" ")
+            game_list.append((inputs[0], inputs[1].strip()))
+    return game_list
+
+
+if __name__ == "__main__":
+    strategy = read_input("Day 2 Rock Paper Scissors/input.txt")
+    print("Normal strategy total points:", calculate_points(strategy))
+    print("Secret strategy total points:", secret_strategy(strategy))
